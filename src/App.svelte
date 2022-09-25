371180f9481ba7ba12cdf6d1b7eef684c6608a95
Davi Santos <94653435+Brisolo32@users.noreply.github.com>
2022-09-25 18:37:36 -0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+
+</script>
+<!-- svelte-ignore a11y-missing-attribute -->
+
+<main>
+  <div>
+    <h1 style="padding-top: 10px;">&#123 Brisolo32 &#125 </h1>
+    <p>I do programming stuff</p>
+  </div>
+  <div class="about">
+    <p>Hi, im Davi &#40 Also known as Brisolo32 &#41, I am currently 13 years old and I program a bit</p>
+    <p>Here is some of the projects I have made: </p>
+  </div>
+  <div class="buttons">
+    <div class="swts" style="padding-bottom: 110px;">
+      <div style="float:left;">
+        <a href="https://github.com/Brisolo32/SwTS">
+          <img src="https://raw.githubusercontent.com/Brisolo32/SwTS/main/swts.png" width="130" height="130">
+        </a>
+      </div>
+      <div class="desc">
+        <span>
+          <b style="
+            font-size: 27px;
+            font-family:'Courier New', Courier, monospace;
+            margin-left: 15px;">SwTS (Sway Theme Switcher)</b>
+        </span>
+        <br>
+        <span style="
+          font-size: 17px;
+          font-family:'Courier New', Courier, monospace;
+          margin-left: 15px;
+          padding-top: 10px;"> A theme/config switcher for SwayWM and Waybar
+        </span>
+      </div>
+    </div>
+    
+
+    <div class="ttc" style="padding-bottom: 110px;">
+      <div style="float:left;">
+        <a href="https://github.com/Brisolo32/TTC">
+          <img src="https://raw.githubusercontent.com/Brisolo32/TTC/main/assets/TTC256.png" width="130" height="130">
+        </a>
+      </div>
+      <div class="desc">
+        <span>
+          <b style="
+            font-size: 27px;
+            font-family:'Courier New', Courier, monospace;
+            margin-left: 15px;">TTC (Twitch Telegram Chat)</b>
+        </span>
+        <br>
+        <span style="
+          font-size: 17px;
+          font-family:'Courier New', Courier, monospace;
+          margin-left: 15px;
+          margin-top: 5px;"> A way to see your Twitch chat inside Telegram
+        </span>
+      </div>
+    </div>
+
+    <div class="gdc" style="">
+      <div style="float:left;">
+        <a href="https://brisolo32.github.io/gdc">
+          <img src="https://raw.githubusercontent.com/Brisolo32/gdc/main/public/favicon.ico" width="130" height="130">
+        </a>
+      </div>
+      <div class="desc">
+        <span>
+          <b style="
+            font-size: 27px;
+            font-family:'Courier New', Courier, monospace;
+            margin-left: 15px;">GDC (GD Song Checker)</b>
+        </span>
+        <br>
+        <span style="
+          font-size: 17px;
+          font-family:'Courier New', Courier, monospace;
+          margin-left: 15px;
+          margin-top: 5px;"> A tool to check if a NewGrounds song is usable on GD
+        </span>
+      </div>
+    </div>
+  </div>
+</main>
+
+<style>
+
+</style>
